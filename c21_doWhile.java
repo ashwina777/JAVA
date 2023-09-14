061a5f8d38f73912ee59d774864eb7722816e408
ASHWINA <114324721+ashwina777@users.noreply.github.com>
2023-09-14 06:38:50 +0530

    Add files via upload
    Do-while loop:
    Do- while loop is similar to a while loop except for the fact that it is guaranteed to execute at least once.
    Use a do-while loop when the exact number of iterations is unknown, but you need to execute a code block at least once.
    After executing a part of a program for once, the rest of the code gets executed on the basis of a given boolean condition.

---
+++ c21_doWhile.java
@@ -0,0 +1,30 @@
+package com.company;
+public class c21_doWhile
+{
+    public static void main(String[] args)
+    {
+        int a = 0;
+        do {
+            System.out.println(a);
+            a++;
+        }while(a<5);
+
+
+        a = 10;
+        do {
+            System.out.println(a);
+            a++;
+        }while(a<5);
+        //main diff b/w while and do while is that do-while executes at least once even if condition is false
+
+        // Quick quiz
+        // print first n natural num using do - while
+        int num = 0;
+                do
+                {
+                    System.out.println(num);
+                    num++;
+
+                }while(num<=100);
+    }
+}
